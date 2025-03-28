cbad4f85e1da265fde93d6cadf28756a28e119f8
ideasgardendev <alessandrosoares1409@gmail.com>
2025-03-28 14:32:02 -0300

    up da main

---
+++ src/components/Card.tsx
@@ -0,0 +1,18 @@
+const Card = () => {
+
+    return(
+        <>
+            <div className="card-front">
+                <img src="#" alt="image not found" />
+                <p>person name</p>
+            </div>
+
+            <div className="card-back">
+                <img src="#" alt="image not found" />
+            </div>
+        </>
+
+    )
+}
+
+export default Card
