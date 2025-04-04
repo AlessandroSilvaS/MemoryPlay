f4f22823592e36f0af89be7a5141b7fa05aba2ad
Francisco Alessandro da Silva Soares <alessandrosoares1409@gmail.com>
2025-04-03 22:18:18 -0300

    03/04/2025

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,16 @@ import Card from './components/Card'
 
 const personNamesArray:Array<string | number> = ['Goku', 'Goku', 'Gohan', 'Gohan', 'Kuririn', 'Kuririn', 'Vegeta', 'Vegeta', 'Piccolo', 'Piccolo', 'shenlong', 'shenlong']
 
-let mixedPersonNamesArray: any = []
+  let mixedPersonNamesArray: any = []
 
-function mixeArray(originArray:Array<string | number>){
+  for(let x:number = personNamesArray.length; x > 0; x--){
 
-  const finalyArray:Array<string | number> = [...originArray]
+    const randomIndex: number = Math.floor(Math.random() * (x + 1))
 
-  for(let x:number = originArray.length - 1; x > 0; x-- ){
-  
-    const randonNumber: number = Math.floor(Math.random() * (x + 1))
-  
-    finalyArray[x], originArray[randonNumber] = finalyArray[randonNumber], originArray[x]
-  
+    mixedPersonNamesArray.push(personNamesArray[randomIndex])
   }
 
-  return finalyArray
 
-}
-
-mixedPersonNamesArray = mixeArray(personNamesArray)
 
 const showCards = mixedPersonNamesArray.map((element: string, index: number) => {
 
@@ -34,10 +25,9 @@ function App() {
 
   return (
     <>
-    <div className="grid">
-      {showCards}
-    </div>
-
+      <div className="grid">
+        {showCards}
+      </div>
     </>
   )
 
